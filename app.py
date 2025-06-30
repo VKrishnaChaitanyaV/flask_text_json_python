fef3e44929fc3f98a7eeb2080fae575f89a6819f
VKrishna <103748294+VKrishnaChaitanyaV@users.noreply.github.com>
2025-06-30 21:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,4 +47,5 @@ def ocr_base64():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's PORT or default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True)
